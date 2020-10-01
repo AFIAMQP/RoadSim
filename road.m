948a8636e7f2a2f92332b0c40023f508857a7248
MeganThrolson <42652883+MeganThrolson@users.noreply.github.com>
2020-10-01 14:44:28 -0400

    had to redo the road to make it a class

--- road.m
+++ road.m
@@ -1,5 +1,21 @@
-function path = road(IRI, numSteps)
-step = normrnd(IRI, (IRI/10), [1,numSteps]);
-
-save path
+classdef road < matlab.System
+    properties 
+       IRI 
+       numSteps
+       IRIArray
+       FlowInput
+    end
+    methods 
+        function newRoad(obj,IRI, numSteps)
+            obj.IRI = IRI;
+            obj.numSteps = numSteps;
+             IRIArray(obj);
+        end
+    
+        function IRIArray(obj)
+            obj.IRIArray = normrnd(obj.IRI, (obj.IRI/10), [1,obj.numSteps]);
+           end
+    end
 end
+   
+
