c65b3169957c3f4ca2012d230756fba4905ed759
MeganThrolson <42652883+MeganThrolson@users.noreply.github.com>
2020-10-10 19:04:22 -0400

    Giving up on MATLAB for the night, Road Class Syntax messed up

--- road.m
+++ road.m
@@ -1,4 +1,4 @@
-classdef road < matlab.System
+classdef road(IRI = IRI numSteps= numSteps) < matlab.System
     properties 
        IRI 
        numSteps
@@ -9,13 +9,28 @@ classdef road < matlab.System
         function newRoad(obj,IRI, numSteps)
             obj.IRI = IRI;
             obj.numSteps = numSteps;
-             IRIArray(obj);
+             IRIArrayCreator(obj);
         end
     
-        function IRIArray(obj)
+        function obj.IRIArray = IRIArrayCreator(obj.IRI,obj.numSteps)
             obj.IRIArray = normrnd(obj.IRI, (obj.IRI/10), [1,obj.numSteps]);
-           end
+        end
+        function Road(obj)
+            i = 1;
+            IRI_0 = obj.IRI;
+            for numSteps
+              if i == 1
+                  IRI_last = IRI_0;
+                  x_last[1] = [0;0;0;0];
+              else
+                  IRI_last = IRIArray[i-1];
+                  x_last[i] = [z.z_s_prime
+                               z.z_s_dot_prime
+                               obj.IRI(i-1)- x_last(i-1)
+                               z.z_u_dot_prime];
+              end
+             Road_step[i] = RoadSolver(IRIArray[i], IRI_last, x_last);
+             obj.input[i] = Road_step[i].R
+            end
+        end
     end
-end
-   
-
